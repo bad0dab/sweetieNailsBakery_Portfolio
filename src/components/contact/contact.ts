5566d2b030095841871ece8433c4bcde23f20cbd
bad0dab <broadnicegamerr@gmail.com>
2026-05-29 19:43:03 +0200

    now sweetienailsbakery@gmail.com gets the form with service, message, user name and user email.

--- src/components/contact/contact.ts
+++ src/components/contact/contact.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, ChangeDetectorRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-declare const emailjs: any;
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
@@ -16,32 +15,48 @@ export class Contact {
   error     = false;
   currentYear = new Date().getFullYear();
 
-  private SERVICE_ID  = 'service_l8ums9n';
-  private TEMPLATE_ID = 'DeVcUcw3keRhgO2t3';
+  private SERVICE_ID         = 'service_l8ums9n';
+  private TEMPLATE_ID_NOTIFY = 'template_g82rvur';
+  private PUBLIC_KEY         = 'DeVcUcw3keRhgO2t3';
 
   name    = '';
   email   = '';
   service = '';
   message = '';
 
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: object,
+    private cdr: ChangeDetectorRef
+  ) {}
+
   async onSubmit(e: Event) {
     e.preventDefault();
+    if (!isPlatformBrowser(this.platformId)) return;
+
     this.sending = true;
     this.error   = false;
+    this.cdr.detectChanges();
 
     try {
-      await emailjs.send(this.SERVICE_ID, this.TEMPLATE_ID, {
-        from_name:  this.name,
-        from_email: this.email,
-        service:    this.service,
-        message:    this.message,
-      });
+      const emailjs = await import('@emailjs/browser');
+      await emailjs.send(
+        this.SERVICE_ID,
+        this.TEMPLATE_ID_NOTIFY,
+        {
+          from_name:  this.name,
+          from_email: this.email,
+          service:    this.service,
+          message:    this.message,
+        },
+        { publicKey: this.PUBLIC_KEY }
+      );
       this.submitted = true;
-    } catch (err) {
-      console.error(err);
+    } catch (err: any) {
+      console.error('EmailJS error:', err);
       this.error = true;
     } finally {
       this.sending = false;
+      this.cdr.detectChanges();
     }
   }
 }
